aa7e689f8801c78c91f20a468aa290a7b0a0ca18
Justin Lim <justlim77@gmail.com>
2016-12-02 02:52:56 +0800

    HTTP POST for Login and ValidateCreds

---
+++ CopierAR/ValidateCreds.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace CopierAR
+{
+    public partial class ValidateCreds : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.Request.HttpMethod == "POST")
+            {
+                string CName = (Request.Form["CName"]);
+
+                RegistrationData rd = DBManager.GetRegistrationData(CName);
+                string jsonResult = JsonConvert.SerializeObject(rd);
+
+                Response.Clear();
+                Response.ContentType = "application/json;";
+                Response.Write(jsonResult);
+                Response.End();
+            }
+        }
+    }
+}
